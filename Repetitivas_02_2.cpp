9fe2fd7b4a25143300b8c3db5f01dd6af197d78e
Ximo Nadal <xnadal@yahoo.es>
2013-05-14 23:36:57 +0200

    practica 2 modulo 1 partes 1 y 2

---
+++ Repetitivas_02_2.cpp
@@ -0,0 +1,55 @@
+// FECHA: 10/05/2013
+// AUTOR:
+// DESCRIPCIÓN:
+// Muestra el funcionamiento de las sentencias repetitivas (0-n) y (1-n).
+// y suma los contadores de las iteraciones (punto 2 de la practica)
+// 
+// ENTRADAS: número de repeticiones (enteros).
+// SALIDAS: texto indicando el número de la iteración.
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+// PROGRAMA PRINCIPAL
+int main()
+{
+  int repe,i,contador;
+
+// EJEMPLO DE SENTENCIA REPETITIVA 1-N
+
+  i=0;
+  contador=0;
+   cout <<"¿cuántas veces quieres repetir el bucle 1-n ?:";
+   cin >> repe;
+
+   do{
+      cout <<"ESTAMOS EN LA REPETICION :" << i + 1 << endl ;
+      contador = contador + i + 1; 
+      cout <<"LA SUMA DE LOS CONTADORES ES :" << contador << endl ;
+      //system("PAUSE");
+      i = i + 1;
+ 
+   } while (i < repe);
+
+// EJEMPLO DE SENTENCIA REPETITIVA 0-N
+
+   i=0;
+   contador=0;
+   cout <<"¿cuántas veces quieres repetir el bucle 0-n ?:";
+   cin >> repe;
+
+   while (i < repe) {
+      cout <<"ESTAMOS EN LA REPETICION :" << i + 1 << endl ;
+      //system("PAUSE");
+      contador = contador + i + 1; 
+      cout <<"LA SUMA DE LOS CONTADORES ES :" << contador << endl ;
+      i = i + 1;
+   }
+
+   cout <<"!! ADIOS !!" << endl ;
+   // system("PAUSE");
+
+   return 0;
+}
+
